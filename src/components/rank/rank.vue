f7b6ff69bc945864968c9457f0e33857738a36d2
cuijianjun <cuijianjun@xiaoyezi.com>
2018-04-08 12:26:16 +0800

    the one first commit

---
+++ src/components/rank/rank.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>排行页面</div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'rank'
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+
+</style>
