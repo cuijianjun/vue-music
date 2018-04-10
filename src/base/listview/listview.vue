b8507d4e4cc7001d986a68e73303748bcfbf6a87
cuijianjun <376247934@qq.com>
2018-04-10 19:21:19 +0800

    listview review

--- src/base/listview/listview.vue
+++ src/base/listview/listview.vue
@@ -95,15 +95,21 @@
       },
       scrollY(newY) {
         const listHeight = this.listHeight
-        for (let i = 0; i < listHeight.length; i++) {
+        // 当滚动到顶部，newY>0
+        if (newY > 0) {
+          this.currentIndex = 0
+          return
+        }
+        for (let i = 0; i < listHeight.length - 1; i++) {
           let height1 = listHeight[i]
           let height2 = listHeight[i + 1]
-          if (!height2 || (-newY > height1 && -newY < height2)) {
+          if (-newY >= height1 && -newY < height2) {
             this.currentIndex = i
             return
           }
         }
-        this.currentIndex = 0
+        // 当滚动到底部。且-newY大于最后一个元素的上限
+        this.currentIndex = listHeight.length - 2
       }
     },
     components: {
