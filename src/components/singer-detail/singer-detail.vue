709bb840c0c6cc8d3bbe549b1a80e4dee04a9000
cuijianjun <376247934@qq.com>
2018-04-12 23:51:39 +0800

    singer detail finished

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -6,13 +6,46 @@
 
 <script type="text/ecmascript-6">
   import {mapGetters} from 'vuex'
+  import {getSingerDetail} from 'api/singer'
+  import {ERR_OK} from 'api/config'
+  import {createSong} from 'common/js/song'
+
   export default {
     name: 'singer-detail',
+    data() {
+      return {
+        songs: []
+      }
+    },
     computed: {
       ...mapGetters(['singer'])
     },
     created() {
+      this._getDetail()
       console.log(this.singer)
+    },
+    methods: {
+      _getDetail() {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
+        }
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+          }
+        })
+      },
+      _normalizeSongs(list) {
+        let ret = []
+        list.forEach((item) => {
+          let {musicData} = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
     }
   }
 </script>
