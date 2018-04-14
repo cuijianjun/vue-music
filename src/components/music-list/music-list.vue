ef8b6f307316219bfcdf0413364712bc393b0ae9
cuijianjun <376247934@qq.com>
2018-04-15 01:37:53 +0800

    music-list (6-11)

--- src/components/music-list/music-list.vue
+++ src/components/music-list/music-list.vue
@@ -3,14 +3,24 @@
     <div class="back">
       <i class="icon-back"></i>
     </div>
-    <h1 class="title"></h1>
-    <div class="bg-image">
+    <h1 class="title" v-html="title"></h1>
+    <div class="bg-image" :style="bgStyle" ref="bgImage">
       <div class="filter"></div>
     </div>
+    <div class="bg-layer" ref="layer"></div>
+    <scroll :data="songs" class="list" ref="list" :probe-type = "probeType" :listen-scroll="listenScroll" @scroll="scroll">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs" ></song-list>
+      </div>
+    </scroll>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import Scroll from 'base/scroll/scroll'
+  import SongList from 'base/song-list/song-list'
+  const RESERVED_HEIGHT = 40
+
   export default {
     name: 'music-list',
     props: {
@@ -32,6 +42,52 @@
         type: Boolean,
         default: false
       }
+    },
+    data() {
+      return {
+        scrollY: 0
+      }
+    },
+    computed: {
+      bgStyle() {
+        return `background-image:url(${this.bgImage})`
+      }
+    },
+    watch: {
+      scrollY(newY) {
+        let zIndex = 0
+        let translateY = Math.max(this.minTranslateY, newY)
+        this.$refs.layer.style['transform'] = `translate3d(0,${translateY}px,0)`
+        this.$refs.layer.style['webkitTransform'] = `translate3d(0,${translateY}px,0)`
+        if (newY < this.minTranslateY) {
+          zIndex = 10
+          this.$refs.bgImage.style.paddingTop = 0
+          this.$refs.bgImage.style.height = `${RESERVED_HEIGHT}px`
+        } else {
+          this.$refs.bgImage.style.paddingTop = `70%`
+          this.$refs.bgImage.style.height = 0
+        }
+        this.$refs.bgImage.style.zIndex = zIndex
+      }
+    },
+    methods: {
+      scroll(pos) {
+        this.scrollY = pos.y
+      }
+    },
+    created() {
+      this.probeType = 3
+      this.listenScroll = true
+    },
+    mounted() {
+      this.imageHeight = this.$refs.bgImage.clientHeight
+      console.log(this.imageHeight)
+      this.minTranslateY = -this.imageHeight + RESERVED_HEIGHT
+      this.$refs.list.$el.style.top = `${this.imageHeight}px`
+    },
+    components: {
+      Scroll,
+      SongList
     }
   }
 </script>
