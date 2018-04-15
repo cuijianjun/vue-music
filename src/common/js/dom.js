1baee99bd03e8af032e477e25f1367ed85d86f52
cuijianjun <376247934@qq.com>
2018-04-15 15:50:38 +0800

    music-list finished

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -21,3 +21,30 @@ export function getData(el, name, val) {
     return el.getAttribute(name)
   }
 }
+
+let elementStyle = document.createElement('div').style
+
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    standard: 'transform'
+  }
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+  if (vendor === 'standard') {
+    return style
+  }
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
+}
