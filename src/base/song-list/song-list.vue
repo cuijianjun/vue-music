6222bb4915e9086de8c29423bcb7903906b5b6db
cuijianjun <376247934@qq.com>
2018-04-16 21:18:31 +0800

    vuex action start

--- src/base/song-list/song-list.vue
+++ src/base/song-list/song-list.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="song-list">
     <ul>
-      <li v-for="(song,index) in songs" class="item" :key="index">
+      <li v-for="(song,index) in songs"
+          class="item" :key="index"
+          @click="selectItem(song, index)"
+      >
         <div class="content">
           <h2 class="name">{{song.name}}</h2>
           <p class="desc">{{getDesc(song)}}</p>
@@ -23,6 +26,9 @@
       }
     },
     methods: {
+      selectItem(item, index) {
+        this.$emit('select', item, index)
+      },
       getDesc(song) {
         return `${song.singer} - ${song.album}`
       }
